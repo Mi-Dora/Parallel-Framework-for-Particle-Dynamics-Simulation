1839aed637a58104648d4f75f8432dd476c3e80a
Yu Du <ydu340@login-coc-ice-2.pace.gatech.edu>
2023-03-30 21:59:05 -0400

    first commit

---
+++ include/user_def.h
@@ -0,0 +1,19 @@
+#ifndef USER_DEF_H
+#define USER_DEF_H
+
+#include <vector>
+#include <cstdint>
+#include <sstream>
+
+
+class Particle{
+    public:
+        double force(Particle other);
+    private:
+    
+}
+
+
+
+
+
