bd9831fbf63685d04fcd00b8bb673dfd10333bc0
QiangWu-Monica <qwu350@gatech.edu>
2023-04-06 22:03:14 -0400

    Add a little GPU framework

---
+++ src/compute_acce.cu
@@ -0,0 +1,8 @@
+#include "kernel.h"
+#include <cuda_runtime.h>
+
+void base_particle::compute_acceleration(Container particles) {
+    // TODO: copy from CPU to GPU
+    // TODO: execute kernel function
+    // TODO: copy from GPU back to CPU
+}
---
+++ src/kernel.cu
@@ -0,0 +1,11 @@
+#include "kernel.h"
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void compute_acceleration(base_particle other) {
+    // TODO: define kernel function
+}
+
+__global__ void compute_acceleration(Container particles) {
+    // TODO: define kernel function
+}
