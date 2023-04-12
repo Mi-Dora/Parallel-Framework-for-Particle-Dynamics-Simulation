5689d164bc0ad4900b80df8ab54430cdb6c76974
Changhai Man <cman8@login-coc-ice-1.pace.gatech.edu>
2023-04-11 21:29:13 -0400

    add load_particles

--- include/user_def/gravity_particle.h
+++ include/user_def/gravity_particle.h
@@ -1,6 +1,7 @@
 #ifndef __GRAVITY_PARTICLE_H__
 #define __GRAVITY_PARTICLE_H__
+#include "particle.h"
 
-void gravityUpdateAcceleration(double* acceleration, double* one, double* another);
+void gravityUpdateAcceleration(particle_t* one, particle_t* another);
 
 #endif
