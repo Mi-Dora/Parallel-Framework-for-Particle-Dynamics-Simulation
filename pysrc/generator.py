47569bad46d3d984a1986eef706f0ac944655894
Changhai Man <changhai0109@hotmail.com>
2023-04-13 20:32:51 +0000

    modified generator.py

--- pysrc/generator.py
+++ pysrc/generator.py
@@ -1,7 +1,8 @@
 import numpy as np
+import argparse
 
 
-def gravity_generator(n_particle, n_dim, pos_range, mass_range):
+def gravity_generator(n_particle, n_dim, pos_range, mass_range, output_filename):
     """
     :param n_particle: int
     :param n_dim: int
@@ -19,12 +20,21 @@ def gravity_generator(n_particle, n_dim, pos_range, mass_range):
     n_feat = np.ones((n_particle, 1))
     mass = np.random.uniform(mass_range[0], mass_range[1], (n_particle, 1))
     mat = np.concatenate((id, pos, vel, acc, n_feat, mass), axis=1)
-    np.savetxt("particle_init.txt", mat, delimiter=' ', newline='\n', header=header, comments='')
+    np.savetxt(output_filename, mat, delimiter=' ', newline='\n', header=header, comments='')
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_particle", type=int, default=1000)
+    parser.add_argument("--n_dim", type=int, default=3)
+    parser.add_argument("--pos_range", type=list, default=[-100, 100, -100, 100])
+    parser.add_argument("--mass_range", type=list, default=[0, 0.01])
+    parser.add_argument("--output_filename", type=str, required=True)
+    
     n_particle = 1000
     n_dim = 3
-    pos_range = [0, 100]
-    mass_range = [0, 0.01]
-    gravity_generator(n_particle, n_dim, pos_range, mass_range)
+    if len(parser.pos_range) == 2:
+        x0, x1 = parser.pos_range[0], parser.pos_range[1]
+        parser.pos_range = [x0, x1, x0, x1]
+    assert len(parser.pos_range) == 4
+    gravity_generator(parser.n_particle, parser.n_dim, parser.pos_range, parser.mass_range, parser.output_filename)
